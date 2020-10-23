83d1bedf64333085f9346be2eb1619be4bf4692b
David Murphy <daveru@Davids-MacBook-Pro.local>
2020-10-23 18:52:59 +0100

    Initial commit - created basic structure and got film list connected to the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <main>
+    <film-list></film-list>
+ </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import FilmList from '@/components/FilmList.vue'
 
 export default {
-  name: 'App',
+  //! THE DATA SECTION OF THIS APP IS DECIDING WHAT THE DATA NAMES AND TYPES WILL BE THAT ARE USED THROUGHOUT THE APP - EG  A LIST OF FILMS
+  data() {
+    return {
+      films: [],
+      characters: []
+    };
+  },
+  //! THE MOUNTED SECTION GRABS WHATEVER INFORMATION IS REQUIRED BEFORE THE APP LOADS - THIS IS DONE ASYNCHRONOUSLY
+  mounted() {
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(res => res.json())
+    .then(data => this.films = data)
+  
+  fetch('https://ghibliapi.herokuapp.com/people')
+    .then(res => res.json())
+    .then(data => this.characters = data)
+  },
   components: {
-    HelloWorld
+    FilmList,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/FilmList.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>This is the Film List</h2>
+</template>
+
+<script>
+export default {
+    //! THIS IS WHERE THE COMPONENT IS DEFINED - 
+    //! IT'S NAME WHICH BECOMES THE HTML TAG IT USES
+    //! THE PROPS IS THE INFOMRATION PASSED TO IT, USED TO POPULATE ITSELF
+    name: "film-list",
+    props: ["films"]
+
+}
+</script>
+
+<style>
+
+</style>
